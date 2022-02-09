69802add458ee6abd33244e9d79ef6baa05bee05
Mrityunjoy Bora <mjborabiz@gmail.com>
2022-02-09 20:27:34 +0530

    Add: Cors code snippit

--- api/index.js
+++ api/index.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 const userUpload = require("./routes/Routes");
 
+app.use((req, res, next) => { 
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With,Content-Type, Authorization, Accept"
+    );
+
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE"
+    );
+  
+    next();
+});
+
 app.use("/user", userUpload)
 
 app.listen(3001, () => { 
